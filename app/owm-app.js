61fd44f7a7c83b3711b0785e9186aa757dd939bf
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-10-25 11:15:54 -0400

    animation beginning

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,36 +1,44 @@
 angular.module('OWMApp', ['ngRoute'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {
-            templateUrl: 'home.html',
-            controller: 'HomeCtrl',
-            controllerAs: 'vm'
-        }).when('/cities/:city', {
-            templateUrl: 'city.html',
-            controller: 'CityCtrl',
-            controllerAs: 'vm',
-            resolve: {
-                city: function(owmCities, $route, $location) {
-                    var city = $route.current.params.city;
-                    if (owmCities.indexOf(city) === -1) {
-                        $location.path('/error');
-                        return;
+                templateUrl: 'home.html',
+                controller: 'HomeCtrl',
+                controllerAs: 'vm'
+            }).when('/cities/:city', {
+                templateUrl: 'city.html',
+                controller: 'CityCtrl',
+                controllerAs: 'vm',
+                resolve: {
+                    city: function(owmCities, $route, $location) {
+                        var city = $route.current.params.city;
+                        if (owmCities.indexOf(city) === -1) {
+                            $location.path('/error');
+                            return;
+                        }
+                        return city;
                     }
-                    return city;
                 }
-            }
-        }).when('/error', {
-        	template: '<p>Error - Page Not Found</p>'
-        })
-        .otherwise('/error');
+            }).when('/error', {
+                template: '<p>Error - Page Not Found</p>'
+            })
+            .otherwise('/error');
     }])
-    .run(['$rootScope', '$location', function($rootScope, $location){
-    	$rootScope.$on('$routeChangeError', function(){
-    		$location.path('/error');
-    	})
-    }])
-	.value('owmCities', ['New York', 'Atlanta', 'Tampa'])
+    .run(function($rootScope, $location, $timeout) {
+        $rootScope.$on('$routeChangeError', function() {
+            $location.path("/error");
+        });
+        $rootScope.$on('$routeChangeStart', function() {
+            $rootScope.isLoading = true;
+        });
+        $rootScope.$on('$routeChangeSuccess', function() {
+            $timeout(function() {
+                $rootScope.isLoading = false;
+            }, 1000);
+        });
+    })
+    .value('owmCities', ['New York', 'Atlanta', 'Tampa'])
     .controller('HomeCtrl', [function() {
-    	//nothing yet
+        //nothing yet
     }])
     .controller('CityCtrl', ['city', function(city) {
         var vm = this;
